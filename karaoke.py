c5c4af1a0c9050dbfa6b8914021244e678a106d7
lyanezgu <lyanezgu@gmail.com>
2014-10-11 22:06:23 +0200

    parte2:lista ordenada

--- karaoke.py
+++ karaoke.py
@@ -16,9 +16,35 @@ def autenticar (argument):
             
             sys.exit("Usage: python karaoke.py file.smile")
     return fich
+def listado (lista):
+    NumTotal = len(lista) / 2
+    print NumTotal
+    Contador = 0
+    posa = 0
+    posb = 1
+    while Contador < NumTotal:
+        etiqueta = lista[posa]
+        val = lista[posb]
+        print etiqueta + "\t",
+
+        for atributo, valor in val.items():
+            if valor != "": # cuabdo hay clave y valor 
+                print atributo + "=" + '"' + valor + '"' + "\t",
+
+        Contador = Contador + 1
+        posa = posa + 2
+        posb = posb + 2
+        print "\n",
+
 
 
 
 if __name__ == "__main__":
     
     fich = autenticar(sys.argv)
+    parser = make_parser()
+    smallHandler = smallsmilhandler.SmallSMILHandler()
+    parser.setContentHandler(smallHandler)
+    parser.parse(open(fich))
+    lista = smallHandler.get_tags()
+    print listado(lista)    
